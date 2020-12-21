f672970d5dd0f630e181ecb8edd86107f93645fc
abdalwahab <bayanadam10@gmail.com>
2020-12-21 05:52:26 +0300

    start the useState hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import ClassState from "./components/classBase/ClassState";
+import HookState from "./components/hooksBase/HookState";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <ClassState /> */}
+      <HookState />
+    </div>
+  );
 }
 
 export default App;
