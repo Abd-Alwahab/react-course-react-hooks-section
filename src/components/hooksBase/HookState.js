606aae4757f5b6e6d3d4df69a79331f73bcc1a78
abdalwahab <bayanadam10@gmail.com>
2020-12-22 16:28:19 +0300

    finish usestate with arrays

--- src/components/hooksBase/HookState.js
+++ src/components/hooksBase/HookState.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
-
+import data from "./../../util/data";
 
 const HookState = () => {
-  return <div></div>;
+  const [books, setBooks] = useState([]);
+
+  const getBooks = () => {
+    setBooks(data);
+  };
+  return (
+    <div>
+      <button onClick={getBooks}>Get Book</button>
+
+      {books.length === 0 ? <div>Loading</div> : books.map((b) => <h3>{b.name}</h3>)}
+    </div>
+  );
 };
 
 export default HookState;
