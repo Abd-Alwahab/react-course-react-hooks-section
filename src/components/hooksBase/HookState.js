e7f4f7229272878ebb0e9e908a79953ce469f011
abdalwahab <bayanadam10@gmail.com>
2020-12-23 14:01:40 +0300

    start useEffect

--- src/components/hooksBase/HookState.js
+++ src/components/hooksBase/HookState.js
@@ -4,12 +4,9 @@ import data from "./../../util/data";
 const HookState = () => {
   const [books, setBooks] = useState([]);
 
-  const getBooks = () => {
-    setBooks(data);
-  };
   return (
     <div>
-      <button onClick={getBooks}>Get Book</button>
+      <button>Get Book</button>
 
       {books.length === 0 ? <div>Loading</div> : books.map((b) => <h3>{b.name}</h3>)}
     </div>
