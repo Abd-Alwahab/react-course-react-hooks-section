eb1c3f8ff1a1440843b04c851e34abf4c5a5a9d6
abdalwahab <bayanadam10@gmail.com>
2020-12-31 21:51:14 +0300

    start with useEffect the first part

--- src/components/hooksBase/HookState.js
+++ src/components/hooksBase/HookState.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import data from "./../../util/data";
 
 const HookState = () => {
   const [books, setBooks] = useState([]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setBooks(data);
+    }, 2000);
+  });
+
   return (
     <div>
-      <button>Get Book</button>
-
       {books.length === 0 ? <div>Loading</div> : books.map((b) => <h3>{b.name}</h3>)}
     </div>
   );
