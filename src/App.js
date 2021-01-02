50155a261f0ea93a929a1f0413929c3ffaf461fb
abdalwahab <bayanadam10@gmail.com>
2021-01-02 11:00:41 +0300

    useContext part 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ClassState from "./components/classBase/ClassState";
 import HookState from "./components/hooksBase/HookState";
 import AuthContext from "./context/auth";
 import data from "./util/data";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <AuthContext.Provider value={{ isLoggedIn: false, user: null, data: data }}>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
       <div className="App">
         <HookState />
       </div>
--- src/components/hooksBase/HookState.js
+++ src/components/hooksBase/HookState.js
@@ -2,11 +2,29 @@ import React, { useContext } from "react";
 import AuthContext from "../../context/auth";
 
 const HookState = () => {
-  const authContext = useContext(AuthContext);
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
 
-  console.log(authContext.data);
+  const login = () => {
+    setIsLoggedIn(true);
+  };
 
-  return <div>{authContext.data}</div>;
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
+  // 00-create a login component
+  // 01-create a lgout component
+
+  return (
+    <div>
+      {isLoggedIn ? <p>LoggedIn</p> : <p> You need to login</p>}
+      {isLoggedIn ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <button onClick={login}>Login</button>
+      )}
+    </div>
+  );
 };
 
 export default HookState;
